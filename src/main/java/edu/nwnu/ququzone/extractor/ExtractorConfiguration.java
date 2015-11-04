20f8353ee71f6afa3725850271c617f50fa2dfa1
ququzone <xueping.yang@gmail.com>
2015-11-04 11:22:29 +0800

    init project

---
+++ src/main/java/edu/nwnu/ququzone/extractor/ExtractorConfiguration.java
@@ -0,0 +1,16 @@
+package edu.nwnu.ququzone.extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * extractor spring configuration.
+ *
+ * @author Yang XuePing
+ */
+@SpringBootApplication
+public class ExtractorConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(ExtractorConfiguration.class, args);
+    }
+}
